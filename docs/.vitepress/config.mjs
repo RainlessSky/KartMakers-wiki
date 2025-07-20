c9a46b09befe84c7dd36e2aed4054c8f35ec20e9
RainlessSky <174295816+RainlessSky@users.noreply.github.com>
2025-07-20 12:39:17 -0700

    Update sidebar

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -24,9 +24,6 @@ export default defineConfig({
           { text: 'Banned blocks', link: '/banned_blocks.md'},
           { text: 'Modified blocks', collapsed: true, items: [
             { text: 'Angle Sensor', link: '/modified_blocks/angle_sensor.md' },
-            { text: 'Gyro Stabilizer', link: '/modified_blocks/gyro_stabilizer.md' },
-            { text: 'Variable Strength Gyro', link: '/modified_blocks/variable_strength_gyro.md' },
-            { text: 'Large Hinge', link: '/modified_blocks/large_hinge.md' },
             { text: 'Piston', link: '/modified_blocks/piston.md' },
             { text: 'Quantum Rudder', link: '/modified_blocks/quantum_rudder.md' },
             { text: 'Seats', link: '/modified_blocks/seats.md' },
